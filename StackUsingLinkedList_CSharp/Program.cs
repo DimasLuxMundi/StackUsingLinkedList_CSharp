421bed9173b6dd5d2a3d775560dbff6345278624
Dimas Damar Buana <dimas.damar.ft21@mail.umy.ac.id>
2022-11-30 16:15:44 +0700

    add Stack class, Stack method, method if top is null, and push method

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -16,4 +16,28 @@ namespace StackUsingLinkedList_CSharp
             next = n;
         }
     }
+    class Stacks
+    {
+        Node top;
+        public Stacks()
+        {
+            top = null;
+        }
+        bool empty()
+        {
+            if (top == null)
+                return true;
+            else
+                return false;
+        }
+        public void push(int element)
+        {
+            Node fresh;
+            fresh = new Node(element, null);
+
+            fresh.next = top;
+            top = fresh;
+            Console.WriteLine("\n" + element + " pushed.");
+        }
+    }
 }
